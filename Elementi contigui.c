1b2969b014e478e8b139ecab091a1f2f626b1cd1
AlmondFlame1287 <mpagano284@gmail.com>
2021-03-05 12:05:49 +0100

    update

--- Elementi contigui.c
+++ Elementi contigui.c
@@ -1,18 +1,39 @@
 /*
-Scrivere un programma C che, 
-ricevendo in ingresso una sequenza di lunghezza arbitraria di numeri interi diversi da zero, 
-terminata da uno zero, 
-produca in uscita la sequenza di tre elementi contigui la cui somma e` massima 
-(se sono presenti piu` sequenze con somma massima, 
+Scrivere un programma C che,
+ricevendo in ingresso una sequenza di lunghezza arbitraria di numeri interi diversi da zero,
+terminata da uno zero,
+produca in uscita la sequenza di tre elementi contigui la cui somma e` massima
+(se sono presenti piu` sequenze con somma massima,
 e` sufficiente stampare la prima).
 */
 
 int main()
 {
-	int somma1 = 0, somma2 = 0;
-	int a, b, c, d, e, f, g, h, i, j;
-	
-	printf("Inserisci una sequenza di numeri: ");
-	scanf("%d %d %d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+	int num = 0, somma1 = 0, somma2 = 0;
+
+	do
+	{
+		printf("Dammi un numero: ");
+		scanf("%d", &num);
+
+		if(num > somma1)
+		{
+			somma1 += num;
+		}
+		else
+		{
+			somma2 += num;
+		}
+
+	} while(num > 0);
+
+	if(somma1 > somma2)
+	{
+		printf(" %d ", somma1);
+	}
+	else
+	{
+		printf(" %d ", somma2);
+	}
 
 }
---
+++ Num numeri.c
@@ -0,0 +1,17 @@
+/*
+Scrivi un programma che faccia la somma di NUM numeri inseriti dall'utente
+*/
+#include <stdio.h>
+
+int main()
+{
+  int num;
+
+  printf("Quanti numeri vuoi addizionare?: ");
+  scanf("%d", &num);
+
+  for(int i; i <= num; i++)
+  {
+
+  }
+}
