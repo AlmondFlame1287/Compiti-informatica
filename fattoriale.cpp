f7c59029525df76999bac9ac3d78cd26dce6aacd
AlmondFlame1287 <mpagano284@gmail.com>
2021-03-06 18:06:07 +0100

    fattoriale

--- fattoriale.cpp
+++ fattoriale.cpp
@@ -2,28 +2,25 @@
 
 int main()
 {
-	int num = 0, num1, fattoriale;
-	
+	int num = 0, fattoriale = 1;
+
 	do
 	{
 		printf("Dammi un numero (inserisci 0 per terminare i numeri da inserire): ");
 		scanf("%d", &num);
-		
-		num1 = num;
-		
+
 		if(num == 0)
 			printf("1");
-		
+
 		else
 		{
-			for(int i = 0; i <= num1; i++)
-			{				
-				fattoriale *= num * (num - 1);
-				num--;	
+			for(int i; i <= num; i++)
+			{
+				fattoriale *= i;
 			}
-			printf("Il fattoriale del numero inserito e': %d\n", fattoriale);
+			printf("Il fattoriale del numero inserito e': %d \n", fattoriale);
 		}
-	
-		
-	} while(num != 0);
+
+
+	} while(num > 0);
 }
