89c97cbd009ff62aaff43bd79662b9f43f971e5b
AlmondFlame1287 <mpagano284@gmail.com>
2021-03-10 08:34:24 +0100

    Aggiornamento gioco con fiche.cpp

--- Gioco con fiche.cpp
+++ Gioco con fiche.cpp
@@ -5,26 +5,56 @@
 
 int inizio(int giocatori, int soldi);
 
-int main() {
-	
-	int nGiocatori;
+int main()
+{
+
+	ushort nGiocatori;
 	int fiches = 1300;
-	
-	do {
-	
+
+	do
+	{
+
 		printf("Quanti giocatori ci saranno?: ");
-		scanf("%d", &nGiocatori);
-		
+		scanf("%hu", &nGiocatori);
+
 		if(nGiocatori <= 0 || nGiocatori == 1)
-			printf("Non puoi giocare con questo numero di giocatori.");
-			
+			printf("Non puoi giocare con questo numero di giocatori. Portati degli amici. \n");
+
 	} while(nGiocatori <= 0 || nGiocatori == 1);
-	
+
 	inizio(nGiocatori, fiches);
 }
 
-int inizio(int giocatori, int soldi) {
-	
-	
-	
+int inizio(ushort giocatori, int soldi)
+{
+	srand(time(NULL));
+	int puntata = 0;
+
+	do
+	{
+		for(ushort i = 1; i <= giocatori; i++)
+		{
+			printf("Giocatore %d, hai a disposizione %d fiches, quanto vuoi puntare?: ", i, soldi);
+			scanf("%d", &puntata);
+
+			do //controllo puntata
+			{
+				if(puntata > soldi)
+				{
+					printf("Non puoi puntare piu' di quello che hai. Prova di nuovo: ");
+					scanf("%d", &puntata);
+				}
+				else if(puntata <= 0)
+				{
+					printf("Non puoi puntare 0 o meno. Prova di nuovo: ");
+					scanf("%d", &puntata);
+				}
+			} while(puntata > soldi || puntata <= 0);
+
+			fiches -= puntata;
+
+		}
+
+	} while(soldi > 0)
+
 }
